3e0422dfa372c619f60ded0f9ef8fedb01c675ac
aneunwook <dmsdnr0720@naver.com>
2025-06-10 13:50:21 +0900

    refactor : Lv2 (N + 1 문제)

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,13 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    /**
+     *
+     * N + 1 문제 해결
+     * 기존 : JOIN FETCH t.user u를 사용하여 조회할 전체 투두와 연관된 user 엔티티를 한번에 조회
+     * 변경: @Query를 @EntityGraph로 수정
+     */
+    @EntityGraph(attributePaths = "user")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
